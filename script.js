0d3cc9f7e48387041990861e5d14543e0b06cf0e
Lapineau-MM <alexzajaczkowski@gmail.com>
2026-09-14 14:46:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const fonts = [
+    "Arial",
+    "Georgia",
+    "Courier New",
+    "Times New Roman",
+    "Helvetica"
+];
+
+let currentFont = 0;
+
+setInterval(() => {
+
+    currentFont++;
+
+    if (currentFont >= fonts.length) {
+        currentFont = 0;
+    }
+
+    document.querySelector("#logo").style.fontFamily =
+        fonts[currentFont];
+
+}, 300);
