6e192fe005a1855d7cd6086f1ab8de13eacdaea9
Lapineau-MM <alexzajaczkowski@gmail.com>
2026-09-15 01:40:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const fonts = [
-    "Arial",
+"Arial",
     "Georgia",
     "Courier New",
     "Times New Roman",
     "Apple Chancery, cursive",
+    "Times New Roman",
+    "Courier New",
+    "Georgia",
     
 ];
 
 let currentFont = 0;
+let intervalDelay = 100;
+let intervalDirection = 1;
+let lowerDelayHoldActive = false;
 
-setInterval(() => {
-
+function changeFont() {
     currentFont++;
 
     if (currentFont >= fonts.length) {
@@ -20,4 +25,33 @@ setInterval(() => {
     document.querySelector("#logo").style.fontFamily =
         fonts[currentFont];
 
-}, 100);
+    intervalDelay += intervalDirection * 100;
+
+    if (intervalDelay >= 1000) {
+        intervalDirection = 0;
+        setTimeout(() => {
+            intervalDirection = -1;
+            setTimeout(changeFont, intervalDelay);
+        }, 5000);
+        return;
+    }
+
+    if (intervalDelay <= 100) {
+        intervalDirection = 0;
+
+        if (!lowerDelayHoldActive) {
+            lowerDelayHoldActive = true;
+            setTimeout(() => {
+                intervalDirection = 1;
+                lowerDelayHoldActive = false;
+            }, 7000);
+        }
+
+        setTimeout(changeFont, 100);
+        return;
+    }
+
+    setTimeout(changeFont, intervalDelay);
+}
+
+setTimeout(changeFont, intervalDelay);
